e2fbff50d6535dea602639c1bde8d46cb237e8e1
Dario Sindicic <dario.sindicic@gmail.com>
2018-10-10 21:57:48 +0200

    solutions of the additional tasks

--- training-exercises/TE01/TrainingExercises.hs
+++ training-exercises/TE01/TrainingExercises.hs
@@ -183,3 +183,39 @@ adjective, noun :: [String]
 -- | Implement a function which takes in a list of pairs / tuples of 'Int's and
 -- returns a list of their sums.
 te133 xs = [fst x + snd x | x <- xs]
+
+-- ** TE 1.1.4 <- EXTRA
+--
+-- | Implement a function which takes in two 'Int's, multiplies them and adds 1 to the result.
+-- Using our trusty IF-THEN-ELSE check if the result of the multiplication is even and in that
+-- case divide it by 2 first. After that add 1 to the resulting number.
+--
+-- Remember that IF-THEN-ELSE is an expression and always returns a value.
+te114 :: Int -> Int -> Int
+te114 x y = if even mul then mulBy2 + 1
+                        else mul + 1
+            where  mul    = x*y
+                   mulBy2 = mul `div` 2
+
+
+-- ** TE 1.2.4 <- EXTRA
+--
+-- | Implement a function which checks if the 'String' is a palindrome. If it is,
+-- then return only the first half (if it has odd length then don't include the
+-- middle character) otherwise return the 'String' unmodified.
+--
+-- You can assume that all letters will be lower case.
+te124 :: String -> String
+te124 s 
+  | s == reverse s = take n s
+  | otherwise      = s
+  where n = (length s) `div` 2
+-- ** TE 1.3.4 <- EXTRA
+--
+-- | Implement a function which takes two sentences as a tuple and returns a list of
+-- common words.
+--
+-- You can assume that all letters are in lower case. Don't forget that you can use
+-- filters in list comprehension.
+te134 :: (String, String) -> [String]
+te134 (x,y) = [ s1 | s1 <- words x, s2 <- words y, s1 == s2]
